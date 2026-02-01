c1d77c1b3fba995c5f489ba200cb46c4efc39908
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2026-02-01 10:54:36 +0530

    feat: Replace BankingScaleHero, CaseStudiesCarousel, and IntegrationCarousel with WhyWeWinSection, PerpetualsSection, and OrderbookSection components in the main page layout

--- web-2/app/page.tsx
+++ web-2/app/page.tsx
@@ -1,8 +1,8 @@
 import { PortfolioNavbar } from "@/components/PortfolioNavbar"
 import { ProductTeaserCard } from "@/components/ProductTeaserCard"
-import { BankingScaleHero } from "@/components/BankingScaleHero"
-import { CaseStudiesCarousel } from "@/components/CaseStudiesCarousel"
-import { IntegrationCarousel } from "@/components/IntegrationCarousel"
+import { WhyWeWinSection } from "@/components/WhyWeWinSection"
+import { PerpetualsSection } from "@/components/PerpetualsSection"
+import { OrderbookSection } from "@/components/OrderbookSection"
 import { PricingSection } from "@/components/PricingSection"
 import { FAQSection } from "@/components/FAQSection"
 import { Footer } from "@/components/Footer"
@@ -12,9 +12,9 @@ export default function Page() {
     <>
       <PortfolioNavbar />
       <ProductTeaserCard />
-      <BankingScaleHero />
-      <CaseStudiesCarousel />
-      <IntegrationCarousel />
+      <WhyWeWinSection />
+      <PerpetualsSection />
+      <OrderbookSection />
       <PricingSection />
       <FAQSection />
       <Footer />
